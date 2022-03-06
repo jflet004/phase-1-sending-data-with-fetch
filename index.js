ce0e6220ade623c22c807f6e2f4be6c814279520
Jose Fletes <joseangelfletes@gmail.com>
2022-03-06 13:36:11 -0800

    commit tree 9069a4fce83f64c9038fd3c8f07c4f0c04ae484b

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(userName, userEmail){
+
+  const userData = {
+    name: `${userName}`,
+    email: `${userEmail}`,
+  }
+  const configurationObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(userData),
+  }
+  
+  //SEND DATA
+  return fetch('http://localhost:3000/users', configurationObj)
+  .then(response => response.json())
+  .then(data => {
+    const element = document.createElement('div');
+    element.textContent = `New id value: ${data.id}`;
+    document.body.appendChild(element);
+  })
+  .catch(error => {
+    const element = document.createElement('div');
+    element.textContent = `Error message: ${error.message}`;
+    document.body.appendChild(element);
+  })
+}
+submitData();
